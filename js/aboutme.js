5ffd7cb1fd77ac6b2c5d1531d91f341742c5ebe5
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2022-07-09 15:43:51 -0400

    Added darkMode() and lightMode()

--- js/aboutme.js
+++ js/aboutme.js
@@ -31,3 +31,18 @@ var THEME_KEY = "corystephenson_theme";
             window.addEventListener("load", function(){
               load_theme();
             });
+
+function darkMode() {
+        var element = document.body;
+        var content = document.getElementById("DarkModetext");
+        element.className = "dark-mode";
+        content.innerText = "Dark Mode is ON";
+}
+      
+
+function lightMode() {
+        var element = document.body;
+        var content = document.getElementById("DarkModetext");
+        element.className = "light-mode";
+        content.innerText = "Dark Mode is OFF";
+}
