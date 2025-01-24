10667868c5f4adfc67e38f664b111a999c3e6c12
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-01-24 16:08:13 -0600

    feat: close dropdown when click off

--- js/Linux.js
+++ js/Linux.js
@@ -16,3 +16,17 @@ for (i = 0; i < dropdown.length; i++) {
   }
   });
 }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
