79da7dd2a2ae017f2d1c96108c571c2401f6deed
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-01-24 18:23:39 -0600

    fix: pasted js code from source again

--- js/Linux.js
+++ js/Linux.js
@@ -1,11 +1,11 @@
 /* Source: https://stackoverflow.com/questions/31932556/simple-toggle-able-js-dropdown-menu */
 $(document).ready(function(){
-    $("[id='myDropdown']").click(function(e) {   
-        $(this).parents(".dropdown").toggleClass("active");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
+    $("[data-toggle='dropdown']").click(function(e) {   
+        $(this).parents(".dropdown").toggleClass("open");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
         e.stopPropagation();
     });
 
     $("html").click(function() {
-        $(".active").removeClass("active");  /*when you click out of the dropdown-menu it remove the class "open"*/
+        $(".open").removeClass("open");  /*when you click out of the dropdown-menu it remove the class "open"*/
     });
 });
