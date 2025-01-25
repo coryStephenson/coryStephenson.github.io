fb37296bcd93df0b878a65396bc3794607708730
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-01-24 18:14:10 -0600

    fix: changed class string to active

--- js/Linux.js
+++ js/Linux.js
@@ -1,11 +1,11 @@
 /* Source: https://stackoverflow.com/questions/31932556/simple-toggle-able-js-dropdown-menu */
 $(document).ready(function(){
     $("[data-toggle='dropdown']").click(function(e) {   
-        $(this).parents(".dropdown").toggleClass("open");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
+        $(this).parents(".dropdown").toggleClass("active");  /*when you click on an element with attr data-toggle='dropdown' it toggle the class "open" on its parent with class "dropdown"*/
         e.stopPropagation();
     });
 
     $("html").click(function() {
-        $(".open").removeClass("open");  /*when you click out of the dropdown-menu it remove the class "open"*/
+        $(".active").removeClass("active");  /*when you click out of the dropdown-menu it remove the class "open"*/
     });
 });
