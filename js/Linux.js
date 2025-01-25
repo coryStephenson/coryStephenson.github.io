a5ac661178ab7d05e6514b6a13b57ba5648352bc
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-01-24 19:16:49 -0600

    Update Linux.js

--- js/Linux.js
+++ js/Linux.js
@@ -1,6 +1,16 @@
-document.addEventListener("click", (event) => {
-  const dropdown = document.getElementById("myDropdown");
-  if (!dropdown.contains(event.target)) {
-    dropdown.style.display = "none";
-  }
-});
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - 
+This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
